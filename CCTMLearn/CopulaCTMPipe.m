da9c7928d541c542edb1a18c9eb2af01ab605685
chenyanxiong <xiongchenyan@gmail.com>
2015-02-03 21:21:12 -0500

    open multiple cores

--- CCTMLearn/CopulaCTMPipe.m
+++ CCTMLearn/CopulaCTMPipe.m
@@ -1,6 +1,8 @@
 function [ flag ] = CopulaCTMPipe(NumOfTopic,OutName)
 %copula pipeline runs
 %data path is hard coded
+matlabpool open 8
+fprintf('using 8 cores\n');
 InName = '/bos/usr0/cx/tmp/CopulaTopicModel/nips12raw_str602.mat';
 load(InName);
 fprintf('load data [%d-%d]:\n',size(counts,2),size(counts,1));
