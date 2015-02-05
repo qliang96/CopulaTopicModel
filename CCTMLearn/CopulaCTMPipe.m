eb7bf468e25482cf8a25ab614790aa061eb7e53f
chenyanxiong <xiongchenyan@gmail.com>
2015-02-04 21:12:53 -0500

    add numof doc para

--- CCTMLearn/CopulaCTMPipe.m
+++ CCTMLearn/CopulaCTMPipe.m
@@ -1,4 +1,4 @@
-function [ flag ] = CopulaCTMPipe(NumOfTopic,OutName)
+function [ flag ] = CopulaCTMPipe(NumOfTopic,NumOfDoc,OutName)
 %copula pipeline runs
 %data path is hard coded
 matlabpool open 8
@@ -8,9 +8,10 @@ load(InName);
 fprintf('load data [%d-%d]:\n',size(counts,2),size(counts,1));
 
 k=ceil(str2double(NumOfTopic));
+m=ceil(str2double(NumOfDoc));
 fprintf('start fitting [%d] topics, output to [%s]\n',k,OutName);
 beta = 0.01;
-[mu,Sigma, Eta,Phi]=CopulaCTM(counts',beta,k);
+[mu,Sigma, Eta,Phi]=CopulaCTM(counts(:,1:m)',beta,k);
 DumpWordTopic(wl,Phi,OutName);
 flag = 1;
 
